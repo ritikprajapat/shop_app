50a69b58615ba60242f9fac8d2dd44457d85e820
ritikprajapat <ritikprajapat.04@gmail.com>
2023-03-31 20:21:50 +0530

    button

--- lib/widgets/badge.dart
+++ lib/widgets/badge.dart
@@ -36,7 +36,7 @@ class Badge extends StatelessWidget {
               value,
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 14,
+                fontSize: 10,
               ),
             ),
           ),
